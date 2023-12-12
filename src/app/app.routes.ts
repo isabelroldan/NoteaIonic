0b77d79b87b8c25a7bcebbcc38b2bb0f38893b0e
Isabel <isabelroldancordoba@hotmail.com>
2023-12-12 10:04:49 +0100

    Configuracion anterior sin hacer commit de creacion de modal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'note-modal',
+    loadComponent: () => import('./modal/note-modal/note-modal.page').then( m => m.NoteModalPage)
+  },
 ];
