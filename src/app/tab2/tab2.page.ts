005d05037214c76d26136b371da7a207fa921efd
Isabel <isabelroldancordoba@hotmail.com>
2023-12-12 10:05:23 +0100

    Ya solo muestra los campos que hay almacenados

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -44,44 +44,6 @@ export class Tab2Page {
             <img src='${note.img}' alt='Imagen de la nota'>
           </div>
           <div *ngIf='note.position'>
-            Latitud: ${note.position?.latitude} | Longitud: ${note.position?.longitude}
-          </div>
-        </div>
-      `,
-      buttons: ['OK']
-    });
-
-    // Obtén el elemento del mensaje
-    const messageElement = await alert.querySelector('.alert-message');
-
-    // Verifica si el elemento existe y no es nulo antes de manipularlo
-    if (messageElement && messageElement.textContent !== null) {
-      // Establece el HTML del mensaje
-      messageElement.innerHTML = messageElement.textContent;
-    }
-
-    await alert.present();
-  }*/
-
-  async presentAlert(note: any) {
-    const alert = await this.alertController.create({
-      header: 'Detalles de la Nota',
-      subHeader: 'A Sub Header Is Optional',
-      message: `
-        <div class="alert-message">
-          <div>
-            ${note.title}
-          </div>
-          <div *ngIf='note.description'>
-            Descripción: ${note.description}
-          </div>
-          <div>
-            Fecha: ${note.date}
-          </div>
-          <div *ngIf='note.img'>
-            <img src='${note.img}' alt='Imagen de la nota'>
-          </div>
-          <div *ngIf='note.position'>
             <div id="map" style="height: 200px;"></div>
           </div>
         </div>
@@ -115,8 +77,79 @@ export class Tab2Page {
       // Añade un marcador en la ubicación
       L.marker([note.position.latitude, note.position.longitude]).addTo(map);
     }
+  }*/
+
+
+  async presentAlert(note: any) {
+  let messageContent = `
+    <div class="alert-message">
+      <div>
+        ${note.title}
+      </div>
+      <div *ngIf='note.description'>
+        Descripción: ${note.description}
+      </div>
+      <div>
+        Fecha: ${note.date}
+      </div>
+  `;
+
+  // Agrega la sección de imagen si hay una imagen en la nota
+  if (note.img) {
+    messageContent += `
+      <div>
+        <img src='${note.img}' alt='Imagen de la nota'>
+      </div>
+    `;
   }
-  
+
+  // Agrega la sección de posición si hay una posición en la nota
+  if (note.position) {
+    messageContent += `
+      <div *ngIf='note.position'>
+        <div id="map" style="height: 200px;"></div>
+      </div>
+    `;
+  }
+
+  // Cierra el div principal
+  messageContent += `</div>`;
+
+  const alert = await this.alertController.create({
+    header: 'Detalles de la Nota',
+    subHeader: 'A Sub Header Is Optional',
+    message: messageContent,
+    buttons: ['OK']
+  });
+
+  // Obtén el elemento del mensaje
+  const messageElement = await alert.querySelector('.alert-message');
+
+  // Verifica si el elemento existe y no es nulo antes de manipularlo
+  if (messageElement && messageElement.textContent !== null) {
+    // Establece el HTML del mensaje
+    messageElement.innerHTML = messageElement.textContent;
+  }
+
+  await alert.present();
+
+  // Obtén el elemento del mapa
+  const mapElement = document.getElementById('map');
+
+  if (mapElement && note.position) {
+    // Crea un mapa Leaflet
+    const map = L.map(mapElement).setView([note.position.latitude, note.position.longitude], 13);
+
+    // Añade una capa de mosaico (puedes ajustar la URL del mosaico según tus necesidades)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // Añade un marcador en la ubicación
+    L.marker([note.position.latitude, note.position.longitude]).addTo(map);
+  }
+}
+
 
 
 
