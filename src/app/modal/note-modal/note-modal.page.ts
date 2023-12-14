9ab7d562da0b8c2c6a213b5e7b1f792cc9e8bfea
Isabel <isabelroldancordoba@hotmail.com>
2023-12-15 00:27:34 +0100

    Añadido al modificar la nota, la eliminacion de la foto y el mapa. Queda afinar

--- src/app/modal/note-modal/note-modal.page.ts
+++ src/app/modal/note-modal/note-modal.page.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Note } from 'src/app/model/note';
 import { ModalController, NavParams } from '@ionic/angular/standalone';
 import { NoteService } from 'src/app/services/note.service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-note-modal',
@@ -13,10 +14,11 @@ import { NoteService } from 'src/app/services/note.service';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule]
 })
-export class NoteModalPage implements OnInit {
+export class NoteModalPage implements OnInit, AfterViewInit {
   @Input() note!: Note;
 
   public noteS: NoteService
+  private map!: L.Map;
 
   constructor(noteS:NoteService, private modalCtrl: ModalController,) {
     this.noteS = noteS;
@@ -26,11 +28,54 @@ export class NoteModalPage implements OnInit {
     console.log(this.note);
   }
 
+  ngAfterViewInit() {
+    if (this.note.position) {
+      this.initializeMap();
+    }
+  }
+
+  initializeMap() {
+    if (!this.map) { // Verificar si el mapa ya se ha inicializado
+      const mapElement = document.getElementById('map');
+      if (mapElement) {
+        this.map = L.map(mapElement).setView([this.note.position?.latitude ?? 0, this.note.position?.longitude ?? 0], 16);
+  
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '© OpenStreetMap contributors'
+        }).addTo(this.map);
+  
+        if (this.note.position?.latitude && this.note.position?.longitude) {
+          L.marker([this.note.position.latitude, this.note.position.longitude]).addTo(this.map);
+        }
+      }
+    }
+  }
+
+  removeMap() {
+    if (this.note.position) {
+      this.note.position.latitude = null;
+      this.note.position.longitude = null;
+    }
+    if (this.map) {
+      this.map.remove();
+    }
+  }
+
+  
+
   cancel() {
     return this.modalCtrl.dismiss(null, 'cancel');
   }
 
   confirm() {
+    console.log("Segundo..."+ this.note.img);
+    console.log("Tercero..."+ this.note);
     return this.modalCtrl.dismiss(this.note, 'confirm');
+  }
+
+  removeImage() {
+    this.note.img = "";
+    console.log("Primero...."+this.note.img);
+
   }
 }
